0f59778e67653dc50c2139cc53a3b7d84e107930
synvatto <dev@synvatto.co>
2022-08-11 17:29:15 +0530

    Update AboutView.vue

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,10 +1,10 @@
 <template>
 	<br><br>
 	<div class="title">
-		<h1>about</h1>
+		<h1>About</h1>
 	</div>
 	<div class="about">
-		<p>hello! i'm synvatto, but you can call me as vatto,<br/> i'm from Sri Lanka and i live in Japan, i love it!<br/> i like to program simple software, linux, and open source!<br/> i program with rust, vue for frontend, and i love lua.</p>
+		<p>hello! i'm synvatto, but you can call me as vatto,<br/> i code and the code works!<br/> i like to program simple software, linux, and open source!<br/> i program with rust, vue for frontend, and i love lua.</p>
 	</div>
 	<br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br>
 </template>
